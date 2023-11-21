bdac335e8789f9c4ba29ac34e95464db18cb5e31
Damjan Vincic <damjanvincic2003@gmail.com>
2023-11-21 13:41:58 +0100

    Add functions to remove stopping words from text

--- internal/simhash/simhash.go
+++ internal/simhash/simhash.go
@@ -1,1 +1,27 @@
 package simhash
+
+import (
+	"slices"
+	"strings"
+)
+
+func isStoppingWord(token string) bool {
+	stoppingWords := []string{"i", "a", "about", "an", "are", "as", "at", "be", "by", "com", "de", "en", "for", "from", "how", "in", "is", "it", "la", "of", "on", "or", "that", "this", "to", "was", "what", "when", "where", "who", "will", "with", "and", "the", "www"}
+	return slices.Contains(stoppingWords, token)
+}
+
+func removeStoppingWords(tokens []string) []string {
+	result := make([]string, 0)
+	for _, token := range tokens {
+		if !isStoppingWord(token) {
+			result = append(result, token)
+		}
+	}
+	return result
+}
+
+func GetFingerprint(text string) string {
+	tokens := strings.Split(text, " ")
+	tokens = removeStoppingWords(tokens)
+	return ""
+}
