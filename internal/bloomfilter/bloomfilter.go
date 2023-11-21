de392f8db75904d2052af672e2fdfdcec0b4eb77
Damjan Vincic <damjanvincic2003@gmail.com>
2023-11-21 23:39:17 +0100

    Add Bloom Filter struct, functions to calculate parameters, and to create an instance

--- internal/bloomfilter/bloomfilter.go
+++ internal/bloomfilter/bloomfilter.go
@@ -1,1 +1,34 @@
 package bloomfilter
+
+import (
+	"github.com/DamjanVincic/key-value-engine/internal/hash"
+	"math"
+)
+
+type BloomFilter struct {
+	m             uint
+	k             uint
+	byteArray     []byte
+	hashFunctions []hash.HashWithSeed
+}
+
+func calculateM(expectedElements int, falsePositiveRate float64) uint {
+	return uint(-float64(expectedElements) * math.Log(falsePositiveRate) / math.Pow(math.Log(2), 2))
+}
+
+func calculateK(expectedElements int, m uint) uint {
+	return uint(float64(m) / float64(expectedElements) * math.Log(2))
+}
+
+func CreateBloomFilter(expectedElements int, falsePositiveRate float64) BloomFilter {
+	m := calculateM(expectedElements, falsePositiveRate)
+	k := calculateK(expectedElements, m)
+
+	bf := BloomFilter{
+		m:             m,
+		k:             k,
+		byteArray:     make([]byte, m),
+		hashFunctions: hash.CreateHashFunctions(k),
+	}
+	return bf
+}
