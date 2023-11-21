a8fe643b10967442b206a0072a74b13bcee24e36
Damjan Vincic <damjanvincic2003@gmail.com>
2023-11-21 13:54:26 +0100

    Replace stop words removal function with a package

--- internal/simhash/simhash.go
+++ internal/simhash/simhash.go
@@ -3,24 +3,9 @@ package simhash
 import (
 	"crypto/md5"
 	"fmt"
-	"slices"
+	"github.com/bbalet/stopwords"
 	"strings"
 )
-
-func isStoppingWord(token string) bool {
-	stoppingWords := []string{"i", "a", "about", "an", "are", "as", "at", "be", "by", "com", "de", "en", "for", "from", "how", "in", "is", "it", "la", "of", "on", "or", "that", "this", "to", "was", "what", "when", "where", "who", "will", "with", "and", "the", "www"}
-	return slices.Contains(stoppingWords, token)
-}
-
-func removeStoppingWords(tokens []string) []string {
-	result := make([]string, 0)
-	for _, token := range tokens {
-		if !isStoppingWord(token) {
-			result = append(result, token)
-		}
-	}
-	return result
-}
 
 func getWeightsForTokens(tokens []string) map[string]int {
 	weights := make(map[string]int)
@@ -40,8 +25,9 @@ func getHashAsString(data string) string {
 }
 
 func GetFingerprint(text string) string {
+	text = stopwords.CleanString(text, "en", false)
+	text = strings.TrimSpace(text)
 	tokens := strings.Split(text, " ")
-	tokens = removeStoppingWords(tokens)
 	weights := getWeightsForTokens(tokens)
 	hashes := make(map[string]string)
 	for _, token := range tokens {
