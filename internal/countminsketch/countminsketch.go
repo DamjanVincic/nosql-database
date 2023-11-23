885d54925d89df7e87ee9ae8aaa79192ea80e720
zoricnadja <zoric.n2003@gmail.com>
2023-11-23 10:57:34 +0100

    Add functions to add an element to cms and get its frequency

--- internal/countminsketch/countminsketch.go
+++ internal/countminsketch/countminsketch.go
@@ -1,5 +1,7 @@
 package main
 
+import "slices"
+
 type CountMinSketch struct {
 	M, K          uint
 	Matrix        [][]uint
@@ -17,4 +19,19 @@ func newCMS(errorRate float64, probability float64) CountMinSketch {
 		}
 	}
 	return CountMinSketch{M: M, K: K, Matrix: Matrix, HashFunctions: CreateHashFunctions(K)}
+}
+func (cms *CountMinSketch) Add(element []byte) {
+	for key, seed := range cms.HashFunctions {
+		value := seed.Hash(element)
+		cms.Matrix[key][value%uint64(cms.M)] += 1
+	}
+}
+func (cms *CountMinSketch) GetFrequency(element []byte) uint {
+	var count = make([]uint, cms.K)
+	for key, seed := range cms.HashFunctions {
+		value := seed.Hash(element)
+		count[key] = cms.Matrix[key][value%uint64(cms.M)]
+	}
+
+	return slices.Min(count)
 }
