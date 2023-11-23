d28d9cee89ebc2a4a23f9bc1061ca819831922f6
zoricnadja <zoric.n2003@gmail.com>
2023-11-23 10:54:54 +0100

    Add function to create an instance

--- internal/countminsketch/countminsketch.go
+++ internal/countminsketch/countminsketch.go
@@ -5,3 +5,16 @@ type CountMinSketch struct {
 	Matrix        [][]uint
 	HashFunctions []HashWithSeed
 }
+
+func newCMS(errorRate float64, probability float64) CountMinSketch {
+	M := uint(CalculateM(errorRate))
+	K := uint(CalculateK(probability))
+	Matrix := make([][]uint, K)
+	for i := range Matrix {
+		Matrix[i] = make([]uint, M)
+		for j := range Matrix[i] {
+			Matrix[i][j] = 0
+		}
+	}
+	return CountMinSketch{M: M, K: K, Matrix: Matrix, HashFunctions: CreateHashFunctions(K)}
+}
