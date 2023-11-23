f6721f870a09b08bb51bd0dc5fa29c6ad36cc178
zoricnadja <zoric.n2003@gmail.com>
2023-11-23 10:59:09 +0100

    Add CountMinSketch serialization

--- internal/countminsketch/countminsketch.go
+++ internal/countminsketch/countminsketch.go
@@ -1,6 +1,10 @@
 package main
 
-import "slices"
+import (
+	"encoding/gob"
+	"os"
+	"slices"
+)
 
 type CountMinSketch struct {
 	M, K          uint
@@ -20,12 +24,14 @@ func newCMS(errorRate float64, probability float64) CountMinSketch {
 	}
 	return CountMinSketch{M: M, K: K, Matrix: Matrix, HashFunctions: CreateHashFunctions(K)}
 }
+
 func (cms *CountMinSketch) Add(element []byte) {
 	for key, seed := range cms.HashFunctions {
 		value := seed.Hash(element)
 		cms.Matrix[key][value%uint64(cms.M)] += 1
 	}
 }
+
 func (cms *CountMinSketch) GetFrequency(element []byte) uint {
 	var count = make([]uint, cms.K)
 	for key, seed := range cms.HashFunctions {
@@ -34,4 +40,36 @@ func (cms *CountMinSketch) GetFrequency(element []byte) uint {
 	}
 
 	return slices.Min(count)
+}
+
+func Serialize(cms *CountMinSketch, filename string) error {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(cms)
+	if err != nil {
+		panic(err.Error())
+	}
+	return nil
+}
+
+func Deserialize(filename string) (*CountMinSketch, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	var cms = new(CountMinSketch)
+	file.Seek(0, 0)
+	for {
+		err = decoder.Decode(cms)
+		if err != nil {
+			break
+		}
+	}
+	return cms, nil
 }
