dafdfb4b751e918931ad118fe33b94f9ddccb476
zoricnadja <zoric.n2003@gmail.com>
2023-11-24 19:53:43 +0100

    edit functions for serialization and deserialization

--- internal/structures/countminsketch/countminsketch.go
+++ internal/structures/countminsketch/countminsketch.go
@@ -1,76 +1,99 @@
 package countminsketch
 
 import (
-	"encoding/gob"
-	"github.com/DamjanVincic/key-value-engine/internal/structures/hash"
-	"os"
+	"encoding/binary"
+	"math"
 	"slices"
+
+	"github.com/DamjanVincic/key-value-engine/internal/structures/hash"
 )
 
 type CountMinSketch struct {
-	M, K          uint
-	Matrix        [][]uint
-	HashFunctions []hash.HashWithSeed
+	m, k          uint32
+	matrix        [][]uint32
+	hashFunctions []hash.HashWithSeed
 }
 
-func newCMS(errorRate float64, probability float64) CountMinSketch {
-	M := uint(CalculateM(errorRate))
-	K := uint(CalculateK(probability))
-	Matrix := make([][]uint, K)
-	for i := range Matrix {
-		Matrix[i] = make([]uint, M)
-		for j := range Matrix[i] {
-			Matrix[i][j] = 0
+func calculateM(epsilon float64) uint32 {
+	return uint32(math.Ceil(math.E / epsilon))
+}
+
+func calculateK(delta float64) uint32 {
+	return uint32(math.Ceil(math.Log(math.E / delta)))
+}
+
+func NewCMS(errorRate float64, probability float64) CountMinSketch {
+	m := uint32(calculateM(errorRate))
+	k := uint32(calculateK(probability))
+	matrix := make([][]uint32, k)
+	for i := range matrix {
+		matrix[i] = make([]uint32, m)
+		for j := range matrix[i] {
+			matrix[i][j] = 0
 		}
 	}
-	return CountMinSketch{M: M, K: K, Matrix: Matrix, HashFunctions: hash.CreateHashFunctions(uint32(K))}
+	return CountMinSketch{m: m, k: k, matrix: matrix, hashFunctions: hash.CreateHashFunctions(uint32(k))}
 }
 
 func (cms *CountMinSketch) Add(element []byte) {
-	for key, seed := range cms.HashFunctions {
+	// Hash the element and increase the value of the corresponding matrix cell by 1
+	for key, seed := range cms.hashFunctions {
 		value := seed.Hash(element)
-		cms.Matrix[key][value%uint32(cms.M)] += 1
+		cms.matrix[key][value%uint32(cms.m)] += 1
 	}
 }
 
-func (cms *CountMinSketch) GetFrequency(element []byte) uint {
-	var count = make([]uint, cms.K)
-	for key, seed := range cms.HashFunctions {
+func (cms *CountMinSketch) GetFrequency(element []byte) uint32 {
+	// Hash the element and return the value of corresponding matrix cell
+	var count = make([]uint32, cms.k)
+	for key, seed := range cms.hashFunctions {
 		value := seed.Hash(element)
-		count[key] = cms.Matrix[key][value%uint32(cms.M)]
+		count[key] = cms.matrix[key][value%uint32(cms.m)]
 	}
-
 	return slices.Min(count)
 }
 
-func Serialize(cms *CountMinSketch, filename string) error {
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer file.Close()
-	encoder := gob.NewEncoder(file)
-	err = encoder.Encode(cms)
-	if err != nil {
-		panic(err.Error())
-	}
-	return nil
-}
+func (cms *CountMinSketch) Serialize() []byte {
+	// Calculate the number of bytes needed to store the byte array and the hash functions
+	//4 + 4 for m and k, for matrix dimensions(m*k)*4 (because type is uint32), and 4*k for hash functions
+	bytes := make([]byte, 8+cms.k*cms.m*4+4*cms.k)
 
-func Deserialize(filename string) (*CountMinSketch, error) {
-	file, err := os.OpenFile(filename, os.O_RDWR, 0666)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	decoder := gob.NewDecoder(file)
-	var cms = new(CountMinSketch)
-	file.Seek(0, 0)
-	for {
-		err = decoder.Decode(cms)
-		if err != nil {
-			break
+	// Append the number of bytes in the byte array
+	binary.BigEndian.PutUint32(bytes[:4], cms.m)
+
+	// Append the number of hash functions
+	binary.BigEndian.PutUint32(bytes[4:8], cms.k)
+
+	// Append the matrix after the first 8 bytes
+	for i := range cms.matrix {
+		for j := range cms.matrix[i] {
+			binary.BigEndian.PutUint32(bytes[8+(i*4+j)*4:12+(i*4+j)*4], cms.matrix[i][j])
 		}
 	}
-	return cms, nil
+
+	// Copy the hash functions after the byte array
+	copy(bytes[8+cms.k*cms.m*4:], hash.Serialize(cms.hashFunctions))
+
+	return bytes
+}
+
+func Deserialize(bytes []byte) CountMinSketch {
+	// First 4 bytes are the number of bytes in the byte array
+	m := binary.BigEndian.Uint32(bytes[:4])
+
+	// Next 4 bytes are the number of hash functions
+	k := binary.BigEndian.Uint32(bytes[4:8])
+
+	// Next m*k*4 bytes is the byte array
+	matrix := make([][]uint32, k)
+	for i := range matrix {
+		matrix[i] = make([]uint32, m)
+		for j := range matrix[i] {
+			matrix[i][j] = binary.BigEndian.Uint32(bytes[8+(i*4+j)*4 : 12+(i*4+j)*4])
+		}
+	}
+	// Next k*4 bytes are the hash functions seeds
+	hashFunctions := hash.Deserialize(bytes[8+k*m*4:])
+
+	return CountMinSketch{m: m, k: k, matrix: matrix, hashFunctions: hashFunctions}
 }
