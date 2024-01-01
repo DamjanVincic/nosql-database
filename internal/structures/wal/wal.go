39be65dbaed0988e84d259d55ac2b758f1338153
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-02 00:22:27 +0100

    Add WAL creation

--- internal/structures/wal/wal.go
+++ internal/structures/wal/wal.go
@@ -1,7 +1,10 @@
 package wal
 
 import (
+	"fmt"
 	"hash/crc32"
+	"os"
+	"path/filepath"
 )
 
 /*
@@ -30,6 +33,10 @@ const (
 	KeySizeStart   = TombstoneStart + TombstoneSize
 	ValueSizeStart = KeySizeStart + KeySizeSize
 	KeyStart       = ValueSizeStart + ValueSizeSize
+
+	//RecordHeaderSize = CrcSize + TimestampSize + TombstoneSize + KeySizeSize + ValueSizeSize
+	path   = "wal"
+	prefix = "wal_"
 )
 
 type Record struct {
@@ -40,6 +47,37 @@ type Record struct {
 	ValueSize uint64
 	Key       string
 	Value     []byte
+}
+
+type WAL struct {
+	segmentSize     uint64
+	currentFilename string
+}
+
+func NewWAL(segmentSize uint64) *WAL {
+	dirEntries, err := os.ReadDir(path)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+	var filename string
+	// If there are no files in the directory, create the first one
+	if len(dirEntries) == 0 {
+		filename = fmt.Sprintf("%s%05d.log", prefix, 1)
+		_, err := os.Create(filepath.Join(path, filename))
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		// Get the last file
+		filename = dirEntries[len(dirEntries)-1].Name()
+	}
+	return &WAL{
+		segmentSize:     segmentSize,
+		currentFilename: filename,
+	}
 }
 
 func CRC32(data []byte) uint32 {
