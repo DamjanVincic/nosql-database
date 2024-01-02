a5a5160e58bd6d37bede28dc240a1ef2cb260fcd
milicaradicc <milica.t.radic@gmail.com>
2024-01-02 18:14:24 +0100

    Add search B-tree

---
+++ internal/structures/b-tree/b-tree.go
@@ -0,0 +1,46 @@
+package b_tree
+
+/*
+implement 2-3 B tree
+every node has at most two keys and three children
+*/
+type BTreeNode struct {
+	keys     []int        // list of keys
+	children []*BTreeNode // list of child pointers
+	leaf     bool         // is node a leaf
+}
+
+func contains(list []int, element int) bool {
+	for _, value := range list {
+		if value == element {
+			return true
+		}
+	}
+	return false
+}
+func init() {
+
+}
+
+// root at first and then recursion, until if finds bigger key
+func search(key int, node *BTreeNode) (bool, *BTreeNode) {
+	if contains(node.keys, key) {
+		return true, node
+	}
+	for _, child := range node.children {
+		found, foundNode := search(key, child)
+		if found {
+			return true, foundNode
+		}
+	}
+	return false, nil
+}
+func Insert(key int, root *BTreeNode) {
+	found, _ := search(key, root)
+	if found {
+		return
+	}
+}
+func Delete() {
+
+}
