e9aaed7b7f98f7180f174defcebc998805e3a240
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-04 15:40:28 +0100

    Add wal comments

--- internal/structures/wal/wal.go
+++ internal/structures/wal/wal.go
@@ -38,11 +38,16 @@ const (
 	ValueSizeStart = KeySizeStart + KeySizeSize
 	KeyStart       = ValueSizeStart + ValueSizeSize
 
+	// RecordHeaderSize The size of the record header
 	RecordHeaderSize = CrcSize + TimestampSize + TombstoneSize + KeySizeSize + ValueSizeSize
-	HeaderSize       = 8
+	// HeaderSize File header size, to store the amount of bytes overflowed from the previous file
+	HeaderSize = 8
 
-	Path         = "wal"
-	Prefix       = "wal_"
+	// Path to store the Write Ahead Log files
+	Path = "wal"
+	// Prefix for the Write Ahead Log files
+	Prefix = "wal_"
+	// LowWaterMark is used to mark the file up until which the logs can be deleted
 	LowWaterMark = "lwm"
 )
 
@@ -51,7 +56,9 @@ type WAL struct {
 	currentFilename string
 }
 
+// NewWAL Create a new Write Ahead Log, segmentSize is the maximum size of a segment in bytes
 func NewWAL(segmentSize uint64) (*WAL, error) {
+	// Create the directory if it doesn't exist
 	dirEntries, err := os.ReadDir(Path)
 	if os.IsNotExist(err) {
 		err := os.Mkdir(Path, os.ModePerm)
@@ -62,12 +69,14 @@ func NewWAL(segmentSize uint64) (*WAL, error) {
 	var filename string
 	// If there are no files in the directory, create the first one
 	if len(dirEntries) == 0 {
+		// Filename format: wal_00001_lwm.log
 		filename = fmt.Sprintf("%s%05d_%s.log", Prefix, 1, LowWaterMark)
 		file, err := os.OpenFile(filepath.Join(Path, filename), os.O_CREATE|os.O_RDWR, 0644)
 		if err != nil {
 			return nil, err
 		}
 
+		// Leave the first HeaderSize bytes for the file header
 		err = file.Truncate(HeaderSize)
 		if err != nil {
 			return nil, err
@@ -82,14 +91,17 @@ func NewWAL(segmentSize uint64) (*WAL, error) {
 	}, nil
 }
 
+// CreateNewSegment Create a new segment file
 func (wal *WAL) CreateNewSegment() error {
 	dirEntries, err := os.ReadDir(Path)
 	if err != nil {
 		return err
 	}
 
+	// Increment the current filename
 	wal.currentFilename = fmt.Sprintf("%s%05d.log", Prefix, len(dirEntries)+1)
 
+	// Create the new file
 	file, err := os.OpenFile(filepath.Join(Path, wal.currentFilename), os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		return err
@@ -101,6 +113,7 @@ func (wal *WAL) CreateNewSegment() error {
 		return err
 	}
 
+	// Leave the first HeaderSize bytes for the file header
 	err = file.Truncate(HeaderSize)
 	if err != nil {
 		return err
@@ -109,13 +122,15 @@ func (wal *WAL) CreateNewSegment() error {
 	return nil
 }
 
+// AddRecord Add a new record to the Write Ahead Log
 func (wal *WAL) AddRecord(key string, value []byte, tombstone bool) error {
 	var record = NewRecord(key, value, tombstone)
 	var recordBytes = record.Serialize()
-	var recordSize = RecordHeaderSize + record.KeySize + record.ValueSize // len(recordBytes) ?
+	var recordSize = uint64(len(recordBytes))
 
+	// Current byte index in the record
 	var idx uint64 = 0
-	for idx < uint64(len(recordBytes)) {
+	for idx < recordSize {
 		file, err := os.OpenFile(filepath.Join(Path, wal.currentFilename), os.O_RDWR, 0644)
 		if err != nil {
 			return err
@@ -127,11 +142,16 @@ func (wal *WAL) AddRecord(key string, value []byte, tombstone bool) error {
 		}
 
 		fileSize := uint64(fileInfo.Size())
+
+		// Remaining bytes in the current file
 		var remainingBytes uint64
+
 		if fileSize-HeaderSize < wal.segmentSize {
 			remainingBytes = wal.segmentSize - (fileSize - HeaderSize)
 		} else {
 			// If segment size got changed in between application runs, and it's smaller than the current file size, we need to create a new segment
+
+			// Close the current file
 			err = file.Close()
 			if err != nil {
 				return err
@@ -157,6 +177,7 @@ func (wal *WAL) AddRecord(key string, value []byte, tombstone bool) error {
 		}
 
 		var mmapFile mmap.MMap
+		// If the record can't fit in the current file
 		if idx+remainingBytes < recordSize {
 			err = file.Truncate(int64(fileSize + remainingBytes))
 			if err != nil {
@@ -186,7 +207,7 @@ func (wal *WAL) AddRecord(key string, value []byte, tombstone bool) error {
 				return err
 			}
 
-			// If the record is bigger than the segment size, it'll take up the entire new segment
+			// If the record bytes are bigger than the segment size, they will take up the entire new segment
 			binary.BigEndian.PutUint64(newSegmentMmap[:HeaderSize], min(recordSize-(idx+remainingBytes), wal.segmentSize))
 			err = newSegmentMmap.Unmap()
 			if err != nil {
@@ -198,6 +219,8 @@ func (wal *WAL) AddRecord(key string, value []byte, tombstone bool) error {
 				return err
 			}
 		} else {
+			// If the record can fit in the current file
+
 			err = file.Truncate(int64(fileSize + recordSize - idx))
 			if err != nil {
 				return err
@@ -228,6 +251,7 @@ func (wal *WAL) AddRecord(key string, value []byte, tombstone bool) error {
 	return nil
 }
 
+// GetRecords Get all records from the Write Ahead Log
 func (wal *WAL) GetRecords() ([]*Record, error) {
 	files, err := os.ReadDir(Path)
 	if err != nil {
@@ -247,11 +271,15 @@ func (wal *WAL) GetRecords() ([]*Record, error) {
 			return nil, err
 		}
 
+		// Offset where the record from previous file ends
 		var offset = int(binary.BigEndian.Uint64(mmapFile[:HeaderSize])) + HeaderSize
+
 		// If the last record got deleted based on low watermark, we will ignore the record's remaining bytes
 		if len(recordBytes) != 0 {
 			recordBytes = append(recordBytes, mmapFile[HeaderSize:offset]...)
 		}
+
+		// If the record bytes overflow the current file, we won't deserialize them yet
 		if len(recordBytes) != 0 && binary.BigEndian.Uint64(mmapFile[:HeaderSize]) != wal.segmentSize {
 			record, err := Deserialize(recordBytes)
 			if err != nil {
@@ -261,18 +289,22 @@ func (wal *WAL) GetRecords() ([]*Record, error) {
 			recordBytes = []byte{}
 		}
 
+		// Current byte index in the file
 		var i = offset
 		for i < len(mmapFile) {
+			// If the current record header size isn't in the same file, we will move on to the next one
 			if i+RecordHeaderSize > len(mmapFile) {
 				break
 			}
 			keySize := binary.BigEndian.Uint64(mmapFile[i+KeySizeStart : i+ValueSizeStart])
 			valueSize := binary.BigEndian.Uint64(mmapFile[i+ValueSizeStart : i+KeyStart])
 
+			// If the record header size fit in this file, but the key or value didn't, we also move on to the next file
 			if i+RecordHeaderSize+int(keySize)+int(valueSize) > len(mmapFile) {
 				break
 			}
 
+			// If the whole record is in this file, we deserialize it
 			record, err := Deserialize(mmapFile[i : uint64(i)+RecordHeaderSize+keySize+valueSize])
 			if err != nil {
 				return nil, err
@@ -281,6 +313,7 @@ func (wal *WAL) GetRecords() ([]*Record, error) {
 			i += RecordHeaderSize + int(record.KeySize+record.ValueSize)
 		}
 
+		// If the record didn't fit in the current file, we store the remaining bytes and move on to the next one
 		recordBytes = append(recordBytes, mmapFile[i:]...)
 
 		err = mmapFile.Unmap()
@@ -315,30 +348,39 @@ func (wal *WAL) FindTimestampSegment(rec *Record) (uint64, error) {
 			return 0, err
 		}
 
+		// Offset where the record from previous file ends
 		var offset = int(binary.BigEndian.Uint64(mmapFile[:HeaderSize])) + HeaderSize
+
 		// If the last record got deleted based on low watermark, we will ignore the record's remaining bytes
 		if len(recordBytes) != 0 {
 			recordBytes = append(recordBytes, mmapFile[HeaderSize:offset]...)
 		}
+
+		// If the record bytes overflow the current file, we won't deserialize them yet
 		if len(recordBytes) != 0 && binary.BigEndian.Uint64(mmapFile[:HeaderSize]) != wal.segmentSize {
 			record, err := Deserialize(recordBytes)
 			if err != nil {
 				return 0, err
 			}
+
+			// If we found a record with the same, or newer timestamp, return the index of the segment
 			if record.Timestamp >= rec.Timestamp {
 				return uint64(idx), nil
 			}
 			recordBytes = []byte{}
 		}
 
+		// Current byte index in the file
 		var i = offset
 		for i < len(mmapFile) {
+			// If the current record header size isn't in the same file, move on to the next one
 			if i+RecordHeaderSize > len(mmapFile) {
 				break
 			}
 			keySize := binary.BigEndian.Uint64(mmapFile[i+KeySizeStart : i+ValueSizeStart])
 			valueSize := binary.BigEndian.Uint64(mmapFile[i+ValueSizeStart : i+KeyStart])
 
+			// If the record header size fit in this file, but the key or value didn't, move on to the next file
 			if i+RecordHeaderSize+int(keySize)+int(valueSize) > len(mmapFile) {
 				break
 			}
@@ -347,12 +389,15 @@ func (wal *WAL) FindTimestampSegment(rec *Record) (uint64, error) {
 			if err != nil {
 				return 0, err
 			}
+
+			// If we found a record with the same, or newer timestamp, return the index of the segment
 			if record.Timestamp >= rec.Timestamp {
 				return uint64(idx), nil
 			}
 			i += RecordHeaderSize + int(record.KeySize+record.ValueSize)
 		}
 
+		// If the record didn't fit in the current file, we store the remaining bytes and move on to the next one
 		recordBytes = append(recordBytes, mmapFile[i:]...)
 
 		err = mmapFile.Unmap()
@@ -368,7 +413,9 @@ func (wal *WAL) FindTimestampSegment(rec *Record) (uint64, error) {
 	return 0, errors.New("no segment found")
 }
 
+// MoveLowWatermark Move the low watermark to the segment that has a record with the given timestamp
 func (wal *WAL) MoveLowWatermark(record *Record) error {
+	// Find the segment that has a record with the given or a newer timestamp
 	idx, err := wal.FindTimestampSegment(record)
 	if err != nil {
 		return err
@@ -379,6 +426,7 @@ func (wal *WAL) MoveLowWatermark(record *Record) error {
 		return err
 	}
 
+	// Find the previous low watermark file and remove the low watermark from it
 	for _, file := range files {
 		parts := strings.SplitN(file.Name(), fmt.Sprintf("_%s", LowWaterMark), 2)
 		if len(parts) != 2 {
@@ -393,9 +441,9 @@ func (wal *WAL) MoveLowWatermark(record *Record) error {
 		break
 	}
 
+	// Rename the segment with the given index to have the low watermark
 	fileName := files[idx].Name()
 	extension := filepath.Ext(fileName)
-	//fileName = strings.Replace(fileName, extension, fmt.Sprintf("_%s%s", LowWaterMark, extension), 1)
 	err = os.Rename(filepath.Join(Path, fileName), filepath.Join(Path, strings.Replace(fileName, extension, fmt.Sprintf("_%s%s", LowWaterMark, extension), 1)))
 	if err != nil {
 		return err
@@ -404,12 +452,14 @@ func (wal *WAL) MoveLowWatermark(record *Record) error {
 	return nil
 }
 
+// DeleteSegments Delete all segments up until the low watermark
 func (wal *WAL) DeleteSegments() error {
 	files, err := os.ReadDir(Path)
 	if err != nil {
 		return err
 	}
 
+	// Delete segments up until the low watermark
 	for _, file := range files {
 		if strings.Contains(file.Name(), LowWaterMark) {
 			break
@@ -425,7 +475,9 @@ func (wal *WAL) DeleteSegments() error {
 		return err
 	}
 
+	// Rename the remaining segments to start from 1
 	for idx, file := range files {
+		// Different logic to rename the low watermark file
 		if strings.Contains(file.Name(), LowWaterMark) {
 			parts := strings.Split(file.Name(), "_")
 			err := os.Rename(filepath.Join(Path, file.Name()), filepath.Join(Path, fmt.Sprintf("%s_%05d_%s", parts[0], idx+1, parts[2])))
