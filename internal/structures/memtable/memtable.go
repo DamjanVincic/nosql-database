3fcfd6bb80e10525bb368534737a1b88172e12b7
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-06 19:15:18 +0100

    Change temp memtable test function

--- internal/structures/memtable/memtable.go
+++ internal/structures/memtable/memtable.go
@@ -2,6 +2,8 @@ package memtable
 
 import (
 	"fmt"
+	"github.com/DamjanVincic/key-value-engine/internal/models"
+	"github.com/DamjanVincic/key-value-engine/internal/structures/hashmap"
 	"github.com/DamjanVincic/key-value-engine/internal/structures/skiplist"
 )
 
@@ -11,8 +13,16 @@ type MemtableData interface {
 	Delete(key string) error
 }
 
-func Test() {
-	var data MemtableData = skipList.CreateSkipList()
+func Test(choice int) {
+	var data MemtableData
+
+	switch choice {
+	case 1:
+		data = skipList.CreateSkipList()
+	case 2:
+		data = hashmap.CreateHashMap()
+	}
+
 	err := data.Put("key1", []byte("value1"), false, 123)
 	if err != nil {
 		return
@@ -26,5 +36,11 @@ func Test() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// Instead of having 3 values, for b tree, hashmap and skip list, we just need to dereference one and everything else remains the same
+	// as if we only had one value
+	memtableValue := (*value).(*models.MemtableValue)
+	fmt.Println(fmt.Sprintf("Value: %s, Tombstone: %t, Timestamp: %d", memtableValue.Value, memtableValue.Tombstone, memtableValue.Timestamp))
+
 	fmt.Println(*value)
 }
