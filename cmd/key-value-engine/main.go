1f8728b97c766416bd79d2f9dcb6cafd26c4b630
milicaradicc <milica.t.radic@gmail.com>
2024-01-07 00:40:28 +0100

    Add summary constant

--- cmd/key-value-engine/main.go
+++ cmd/key-value-engine/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	skipList "github.com/DamjanVincic/key-value-engine/internal/structures/skiplist"
+	"github.com/DamjanVincic/key-value-engine/internal/structures/sstable"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	memEntries := getMemEntry()
+	sstable.NewSSTable2(memEntries, 50)
+
+}
+func getMemEntry() []sstable.MemEntry {
+	sampleSkipListValue := &skipList.SkipListValue{
+		Value:     []byte("example value"),
+		Tombstone: false,
+		Timestamp: 123456789,
+	}
+
+	// Create 50 MemEntry instances with the same SkipListValue
+	memEntries := make([]sstable.MemEntry, 50)
+	for i := 0; i < 50; i++ {
+		memEntries[i] = sstable.MemEntry{
+			Key:   fmt.Sprintf("Key%d", i),
+			Value: sampleSkipListValue,
+		}
+	}
+
+	return memEntries
 }
