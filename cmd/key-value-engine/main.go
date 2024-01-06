bd613e3fe070d863af077259e6b023c9a8be5d10
milicaradicc <milica.t.radic@gmail.com>
2024-01-07 00:41:24 +0100

    Return old main

--- cmd/key-value-engine/main.go
+++ cmd/key-value-engine/main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"fmt"
-	skipList "github.com/DamjanVincic/key-value-engine/internal/structures/skiplist"
-	"github.com/DamjanVincic/key-value-engine/internal/structures/sstable"
-)
+import "fmt"
 
 func main() {
-	memEntries := getMemEntry()
-	sstable.NewSSTable2(memEntries, 50)
-
-}
-func getMemEntry() []sstable.MemEntry {
-	sampleSkipListValue := &skipList.SkipListValue{
-		Value:     []byte("example value"),
-		Tombstone: false,
-		Timestamp: 123456789,
-	}
-
-	// Create 50 MemEntry instances with the same SkipListValue
-	memEntries := make([]sstable.MemEntry, 50)
-	for i := 0; i < 50; i++ {
-		memEntries[i] = sstable.MemEntry{
-			Key:   fmt.Sprintf("Key%d", i),
-			Value: sampleSkipListValue,
-		}
-	}
-
-	return memEntries
+	fmt.Println("Hello World!")
 }
