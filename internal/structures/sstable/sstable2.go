1418b391ae617acde80c077089cab84764867d53
zoricnadja <zoric.n2003@gmail.com>
2024-01-06 16:50:27 +0100

    add SSTable struct to SSTable

--- internal/structures/sstable/sstable2.go
+++ internal/structures/sstable/sstable2.go
@@ -37,3 +37,13 @@ type MemEntry struct {
 	Key   string
 	Value *skipList.SkipListValue
 }
+
+type SSTable struct {
+	tableSize            uint64
+	dataFilename         string
+	indexFilename        string
+	summaryIndexFilename string
+	filterFilename       string
+	metadataFilename     string
+	tocFilename          string
+}
