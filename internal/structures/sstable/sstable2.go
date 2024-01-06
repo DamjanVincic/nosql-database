aab5316e5054bd0bc685d2debf82146ecb548a39
zoricnadja <zoric.n2003@gmail.com>
2024-01-06 18:42:21 +0100

    add function for creating new SSTable

--- internal/structures/sstable/sstable2.go
+++ internal/structures/sstable/sstable2.go
@@ -1,6 +1,11 @@
 package sstable
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+
 	skipList "github.com/DamjanVincic/key-value-engine/internal/structures/skiplist"
 )
 
@@ -34,6 +39,14 @@ const (
 
 	// Path to store the SSTable files
 	Path = "sstable"
+	// File naming constants
+	Prefix       = "sst_"
+	DataSufix    = "_data"
+	IndexSufix   = "_index"
+	SummarySufix = "_summary"
+	FilterSufix  = "_filter"
+	MetaSufix    = "_meta"
+	TocSufix     = "_toc"
 )
 
 type MemEntry struct {
@@ -49,4 +62,97 @@ type SSTable struct {
 	filterFilename       string
 	metadataFilename     string
 	tocFilename          string
+}
+
+func NewSSTable(tableSize uint64) (*SSTable, error) {
+	// Create the directory if it doesn't exist
+	dirEntries, err := os.ReadDir(Path)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(Path, os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+	}
+	var dataFilename string
+	var indexFilename string
+	var summaryFilename string
+	var filterFilename string
+	var metadataFilename string
+	var tocFilename string
+
+	lsmIndex := uint8(1)
+	index := uint8(1)
+	var subdirPath string
+	var subdirName string
+
+	// If there are no subdirectoriums in the directory, create the first one
+	if len(dirEntries) == 0 {
+		// subdirName : sstable/sstableN (N - index)
+		subdirName = fmt.Sprintf("sstable%d", index)
+		subdirPath = filepath.Join(Path, subdirName)
+		err := os.Mkdir(subdirPath, os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// Get the last file
+		subdirName = dirEntries[len(dirEntries)-1].Name()
+		n, err := strconv.ParseUint(subdirName[15:], 10, 8)
+		index = uint8(n)
+		if err != nil {
+			return nil, err
+		}
+		index += 1
+		subdirName = fmt.Sprintf("sstable%d", index)
+		subdirPath = filepath.Join(Path, subdirName)
+
+	}
+	// Filename format: sst_00001_lsmi_PART.db
+	dataFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, DataSufix)
+	indexFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, IndexSufix)
+	summaryFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, SummarySufix)
+	filterFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, FilterSufix)
+	metadataFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, MetaSufix)
+	tocFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, TocSufix)
+
+	dataFile, err := os.OpenFile(filepath.Join(subdirPath, dataFilename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	indexFile, err := os.OpenFile(filepath.Join(subdirPath, indexFilename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	summaryFile, err := os.OpenFile(filepath.Join(subdirPath, summaryFilename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	filterFile, err := os.OpenFile(filepath.Join(subdirPath, filterFilename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	metadataFile, err := os.OpenFile(filepath.Join(subdirPath, metadataFilename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	tocFile, err := os.OpenFile(filepath.Join(subdirPath, tocFilename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	dataFile.Close()
+	indexFile.Close()
+	summaryFile.Close()
+	filterFile.Close()
+	metadataFile.Close()
+	tocFile.Close()
+
+	return &SSTable{
+		tableSize:            tableSize,
+		dataFilename:         dataFilename,
+		indexFilename:        indexFilename,
+		summaryIndexFilename: summaryFilename,
+		filterFilename:       filterFilename,
+		metadataFilename:     metadataFilename,
+		tocFilename:          tocFilename,
+	}, nil
 }
