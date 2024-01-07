e40e334115c6af52c6d8ba892cf713b3fe8d411f
milicaradicc <milica.t.radic@gmail.com>
2024-01-07 20:32:14 +0100

    Remove file

--- cmd/key-value-engine/main.go
+++ cmd/key-value-engine/main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/DamjanVincic/key-value-engine/internal/structures/merkle"
-	"github.com/DamjanVincic/key-value-engine/internal/structures/skipList"
-	"os"
-)
+import "fmt"
 
 func main() {
-	key1 := "1"
-	key2 := "2"
-	key3 := "3"
-	key4 := "4"
-	key5 := "5"
-	value1 := skipList.SkipListValue{[]byte{1}, true, 1}
-	value2 := skipList.SkipListValue{[]byte{2}, true, 2}
-	value3 := skipList.SkipListValue{[]byte{3}, true, 3}
-	value4 := skipList.SkipListValue{[]byte{4}, true, 4}
-	value5 := skipList.SkipListValue{[]byte{5}, true, 5}
-
-	dataDict := map[string]skipList.SkipListValue{
-		key1: value1,
-		key2: value2,
-		key3: value3,
-		key4: value4,
-		key5: value5,
-	}
-	tree := merkle.CreateMerkleTree(dataDict)
-	root := tree.Root
-	var result [][]byte
-	result = merkle.LevelOrder(root)
-	fmt.Println(result)
-
-	err := os.Remove("dgf.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = tree.WriteInFile(result, "dgf.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	treeNew, err := tree.ReadFromFile("dgf.txt")
-	//fmt.Println(result)
-	if tree == treeNew {
-		fmt.Println("true")
-	} else {
-		fmt.Println("false")
-	}
+	fmt.Println("Hello World!")
 }
