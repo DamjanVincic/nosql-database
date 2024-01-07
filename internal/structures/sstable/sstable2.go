98b91ce1812b92bda8c078bfa44f7f73d69018cb
zoricnadja <zoric.n2003@gmail.com>
2024-01-07 02:31:23 +0100

    add SimpleSSTable structure

--- internal/structures/sstable/sstable2.go
+++ internal/structures/sstable/sstable2.go
@@ -37,9 +37,11 @@ const (
 	KeySizeStart = 0
 	KeyStart     = KeySizeStart + KeySizeSize
 
-	// Path to store the SSTable files
+	// Path to store SSTable files
 	Path = "sstable"
-	// File naming constants
+	// Path to store the SimpleSStable file
+	SimplePath = "simplesstable"
+	// File naming constants for SSTable
 	Prefix       = "sst_"
 	DataSufix    = "_data"
 	IndexSufix   = "_index"
@@ -47,6 +49,8 @@ const (
 	FilterSufix  = "_filter"
 	MetaSufix    = "_meta"
 	TocSufix     = "_toc"
+	// File naming constants for simpleSSTable
+	SimplePrefix = "ssst_"
 )
 
 type MemEntry struct {
@@ -65,7 +69,53 @@ type SSTable struct {
 	tocFilename          string
 }
 
-func NewSSTable(tableSize uint64) (*SSTable, error) {
+type SimpleSSTable struct {
+	tableSize uint64
+	filename  string
+}
+
+func NewSimpleSSTable(memEntries []MemEntry, tableSize uint64) (*SimpleSSTable, error) {
+	// Create the directory if it doesn't exist
+	dirEntries, err := os.ReadDir(SimplePath)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(SimplePath, os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+	}
+	var filename string
+	var index uint8
+	lsmIndex := uint8(1)
+
+	// If there are no files in the directory, create the first one
+	if len(dirEntries) == 0 {
+		index = 1
+		// Filename format: ssst_00001_lsmindex.log
+	} else {
+		// Get the last file
+		filename = dirEntries[len(dirEntries)-1].Name()
+		n, err := strconv.ParseUint(filename[5:10], 10, 32)
+		index = uint8(n)
+		if err != nil {
+			return nil, err
+		}
+		index++
+	}
+	filename = fmt.Sprintf("%s%05d_%d.db", SimplePrefix, index, lsmIndex)
+	file, err := os.OpenFile(filepath.Join(SimplePath, filename), os.O_CREATE|os.O_RDWR, 0644)
+	fmt.Print(file)
+	if err != nil {
+		return nil, err
+	}
+
+	file.Close()
+	return &SimpleSSTable{
+		tableSize: tableSize,
+		filename:  filename,
+	}, nil
+
+}
+func NewSSTable(memEntries []MemEntry, tableSize uint64) (*SSTable, error) {
 	// Create the directory if it doesn't exist
 	dirEntries, err := os.ReadDir(Path)
 	if os.IsNotExist(err) {
@@ -82,39 +132,39 @@ func NewSSTable(tableSize uint64) (*SSTable, error) {
 	var tocFilename string
 
 	lsmIndex := uint8(1)
-	index := uint8(1)
+	//index indicates the order in which the directories(sstables) were created
+	var index uint32
 	var subdirPath string
 	var subdirName string
 
+	// subdirName : sstableN (N - index)
 	// If there are no subdirectoriums in the directory, create the first one
-	if len(dirEntries) == 0 {
-		// subdirName : sstable/sstableN (N - index)
-		subdirName = fmt.Sprintf("sstable%d", index)
-		subdirPath = filepath.Join(Path, subdirName)
-		err := os.Mkdir(subdirPath, os.ModePerm)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		// Get the last file
-		subdirName = dirEntries[len(dirEntries)-1].Name()
-		n, err := strconv.ParseUint(subdirName[15:], 10, 8)
-		index = uint8(n)
-		if err != nil {
-			return nil, err
-		}
-		index += 1
-		subdirName = fmt.Sprintf("sstable%d", index)
-		subdirPath = filepath.Join(Path, subdirName)
 
+	if len(dirEntries) == 0 {
+		index = 1
+	} else {
+		// Get the index from last added file and inkrement it by one
+		subdirName = dirEntries[len(dirEntries)-1].Name()
+		n, err := strconv.ParseUint(subdirName[7:], 10, 32)
+		index = uint32(n)
+		if err != nil {
+			return nil, err
+		}
+		index++
+	}
+	subdirName = fmt.Sprintf("sstable%d", index)
+	subdirPath = filepath.Join(Path, subdirName)
+	err = os.Mkdir(subdirPath, os.ModePerm)
+	if err != nil {
+		return nil, err
 	}
 	// Filename format: sst_00001_lsmi_PART.db
-	dataFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, DataSufix)
-	indexFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, IndexSufix)
-	summaryFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, SummarySufix)
-	filterFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, FilterSufix)
-	metadataFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, MetaSufix)
-	tocFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, 1, lsmIndex, TocSufix)
+	dataFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, index, lsmIndex, DataSufix)
+	indexFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, index, lsmIndex, IndexSufix)
+	summaryFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, index, lsmIndex, SummarySufix)
+	filterFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, index, lsmIndex, FilterSufix)
+	metadataFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, index, lsmIndex, MetaSufix)
+	tocFilename = fmt.Sprintf("%s%05d_%d%s.db", Prefix, index, lsmIndex, TocSufix)
 
 	dataFile, err := os.OpenFile(filepath.Join(subdirPath, dataFilename), os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
@@ -140,6 +190,14 @@ func NewSSTable(tableSize uint64) (*SSTable, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// offset that points to begining of file
+	offset := uint64(0)
+	for _, entry := range memEntries {
+		offset, _ = addToDataSegment(dataFile, entry)
+		addToSparseIndex(indexFile, entry, offset)
+	}
+
 	dataFile.Close()
 	indexFile.Close()
 	summaryFile.Close()
