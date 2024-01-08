a6c63fd3578f31c8061f7da098bdde878f69b1ba
zoricnadja <zoric.n2003@gmail.com>
2024-01-08 19:10:32 +0100

    fix merkle implementation errors

--- internal/structures/merkle/merkle.go
+++ internal/structures/merkle/merkle.go
@@ -17,7 +17,7 @@ const (
 
 type MerkleTree struct {
 	Root         *Node
-	hashWithSeed []hash.HashWithSeed
+	hashWithSeed hash.HashWithSeed
 }
 
 type Node struct {
@@ -58,9 +58,8 @@ func createDataForNode(key string, data *models.Data) []byte {
 /*
 get binary data and hash function and return new node with no children and hashed values
 */
-func createNewNode(key string, value *models.Data) *Node {
+func createNewNode(key string, value *models.Data, hashFunc hash.HashWithSeed) *Node {
 	newData := createDataForNode(key, value)
-	hashFunc := hash.CreateHashFunctions(1)[0]
 	values, _ := hashFunc.Hash(newData)
 	valuesBinary := make([]byte, 8)
 	binary.BigEndian.PutUint64(valuesBinary, values)
@@ -75,10 +74,10 @@ there hash wont change anything
 func CreateMerkleTree(allData map[string]*models.Data) *MerkleTree {
 	var nodes []*Node
 	var merkleTree MerkleTree
-
+	merkleTree.hashWithSeed = hash.CreateHashFunctions(1)[0]
 	// creating all the end nodes
 	for key, data := range allData {
-		node := createNewNode(key, data)
+		node := createNewNode(key, data, merkleTree.hashWithSeed)
 		nodes = append(nodes, node)
 	}
 
@@ -89,18 +88,17 @@ func CreateMerkleTree(allData map[string]*models.Data) *MerkleTree {
 		targetSize := int(math.Pow(2, degree)) // ex. n is 2.3 then degree is 3 and we need 8 nodes since 2**3
 		for i := len(nodes); i < targetSize; i++ {
 			// add number of empty nodes that is needed
-			nodes = append(nodes, createNewNode("", &models.Data{Value: []byte{}, Tombstone: true, Timestamp: 0}))
+			nodes = append(nodes, createNewNode("", &models.Data{Value: []byte{}, Tombstone: true, Timestamp: 0}, merkleTree.hashWithSeed))
 		}
 	}
 
 	for len(nodes) > 1 {
 		var newLevel []*Node
 
-		merkleTree.hashWithSeed = hash.CreateHashFunctions(1)
+		merkleTree.hashWithSeed = hash.CreateHashFunctions(1)[0]
 		for i := 0; i < len(nodes); i += 2 {
-			hashFunc := merkleTree.hashWithSeed[0]
 			// add two array together and hash
-			values, _ := hashFunc.Hash(append(nodes[i].data, nodes[i+1].data...))
+			values, _ := merkleTree.hashWithSeed.Hash(append(nodes[i].data, nodes[i+1].data...))
 			valuesBinary := make([]byte, 8)
 			binary.BigEndian.PutUint64(valuesBinary, values)
 			// create new node with nodes at index i and i + 1, parent-left and right child
@@ -121,9 +119,9 @@ func CreateMerkleTree(allData map[string]*models.Data) *MerkleTree {
 func (tree *MerkleTree) Serialize() []byte {
 	//add size of hashWithSeed to byte array
 	bytes := make([]byte, HashWithSeedSizeSize)
-	serializedHash := hash.Serialize(tree.hashWithSeed)
+	serializedHash := hash.Serialize([]hash.HashWithSeed{tree.hashWithSeed})
 	hashFuncSize := uint64(len(serializedHash))
-	binary.BigEndian.PutUint64(bytes[:8], hashFuncSize)
+	binary.BigEndian.PutUint64(bytes[:HashWithSeedSizeSize], hashFuncSize)
 	//append hashWithSeed
 	bytes = append(bytes, serializedHash...)
 	bytes = append(bytes, MerkleBFS(tree.Root)...)
@@ -135,7 +133,7 @@ func MerkleBFS(root *Node) []byte {
 	if root == nil {
 		return []byte{}
 	}
-	result := []byte{}
+	result := make([]byte, 0)
 	nodes := []*Node{root}
 	for len(nodes) > 0 {
 		current := nodes[0] // get next in line
@@ -154,10 +152,10 @@ func MerkleBFS(root *Node) []byte {
 func DeserializeMerkle(data []byte) (*MerkleTree, error) {
 	//deserialize hash func
 	hashWithSeedSize := binary.BigEndian.Uint64(data[:HashWithSeedSizeSize])
-	hashWithSeed := hash.Deserialize(data[HashWithSeedSizeSize : HashWithSeedSizeSize+hashWithSeedSize])
+	hashWithSeed := hash.Deserialize(data[HashWithSeedSizeSize : HashWithSeedSizeSize+hashWithSeedSize])[0]
 	//deserialize nodes
 	var nodes []byte
-	for offset := uint64(HashWithSeedSizeSize + hashWithSeedSize); offset < uint64(len(data)); offset += HashedNodesSize {
+	for offset := HashWithSeedSizeSize + hashWithSeedSize; offset < uint64(len(data)); offset += HashedNodesSize {
 		nodes = append(nodes, data[offset:offset+HashedNodesSize]...)
 	}
 	//constructs merkle from list of nodes and returns root node
