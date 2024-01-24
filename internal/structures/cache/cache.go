f42dff90394baedff785a073ff1dcf45527ca6cc
Mijat Krivokapić <mijat2004@gmail.com>
2024-01-24 18:33:48 +0100

    implement Put

--- internal/structures/cache/cache.go
+++ internal/structures/cache/cache.go
@@ -18,8 +18,18 @@ func NewCache() *Cache {
 	return &Cache{cacheList: list.New(), cacheMap: make(map[string]*list.Element)}
 }
 
-func (cache *Cache) Put(key string) {
-
+func (cache *Cache) Put(entry *models.MemEntry) {
+	elem, ok := cache.cacheMap[entry.Key]
+	if !ok {
+		cache.cacheList.PushFront(entry)
+		if cache.cacheList.Len() > capacity {
+			elem = cache.cacheList.Back()
+			delete(cache.cacheMap, elem.Value.(*models.MemEntry).Key)
+			cache.cacheList.Remove(elem)
+		}
+	} else {
+		cache.cacheList.MoveToFront(elem)
+	}
 }
 
 func (cache *Cache) Get(key string) *models.MemEntry {
