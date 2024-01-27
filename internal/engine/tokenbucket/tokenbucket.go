b1a4776955ceb819868638f595cf54f39269c18e
Mijat Krivokapić <mijat2004@gmail.com>
2024-01-27 16:17:33 +0100

    add variable encoding

--- internal/engine/tokenbucket/tokenbucket.go
+++ internal/engine/tokenbucket/tokenbucket.go
@@ -33,24 +33,25 @@ func (tokenBucket *TokenBucket) UseToken() bool {
 
 func (tokenBucket *TokenBucket) Serialize() []byte {
 	serializedTokenBucket := make([]byte, 0)
-	// Temporary storage for 64-bit integers
-	tempByte := make([]byte, 8)
+
+	tempByte := make([]byte, binary.MaxVarintLen64) // Temporary storage for 64-bit integers
+	var tempBytesWritten int
 
 	//convert tokenCount to byte array and append it to serializedTokenBucket
-	binary.BigEndian.PutUint64(tempByte, tokenBucket.tokenCount)
-	serializedTokenBucket = append(serializedTokenBucket, tempByte...)
+	tempBytesWritten = binary.PutUvarint(tempByte, tokenBucket.tokenCount)
+	serializedTokenBucket = append(serializedTokenBucket, tempByte[:tempBytesWritten]...)
 
 	//convert lastRefillTime to byte array and append it to serializedTokenBucket
-	binary.BigEndian.PutUint64(tempByte, tokenBucket.lastRefillTime)
-	serializedTokenBucket = append(serializedTokenBucket, tempByte...)
+	tempBytesWritten = binary.PutUvarint(tempByte, tokenBucket.lastRefillTime)
+	serializedTokenBucket = append(serializedTokenBucket, tempByte[:tempBytesWritten]...)
 
 	return serializedTokenBucket
 }
 
 func Deserialize(serializedTokenBucket []byte) *TokenBucket {
 	//get tokenCount from first 8 bytes of serializedTokenBucket
-	tokenCount := binary.BigEndian.Uint64(serializedTokenBucket[:8])
+	tokenCount, bytesRead := binary.Uvarint(serializedTokenBucket)
 	//get lastRefillTime from second 8 bytes of serializedTokenBucket
-	lastRefillTime := binary.BigEndian.Uint64(serializedTokenBucket[8:])
+	lastRefillTime, _ := binary.Uvarint(serializedTokenBucket[bytesRead:])
 	return &TokenBucket{tokenCount: tokenCount, lastRefillTime: lastRefillTime}
 }
