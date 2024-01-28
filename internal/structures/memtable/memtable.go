b61e56ef8575ad290790114e8b50f4362efcd538
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-28 15:24:17 +0100

    Prevent memtable from returning deleted elements

--- internal/structures/memtable/memtable.go
+++ internal/structures/memtable/memtable.go
@@ -83,13 +83,19 @@ func (memtable *Memtable) Get(key string) *models.Data {
 
 	data = memtable.currentPartition.Get(key)
 	if data != nil {
-		return data
+		if !data.Tombstone {
+			return data
+		}
+		return nil
 	}
 
 	for i := len(memtable.partitions) - 1; i >= 0; i-- {
 		data = memtable.partitions[i].Get(key)
 		if data != nil {
-			return data
+			if !data.Tombstone {
+				return data
+			}
+			return nil
 		}
 	}
 	return nil
