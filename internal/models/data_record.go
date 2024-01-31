5e9dda408113785d1b213d9b27acf2a1f08f566f
Damjan Vincic <damjanvincic2003@gmail.com>
2024-01-31 15:21:05 +0100

    Refactor DataRecord

--- internal/models/data_record.go
+++ internal/models/data_record.go
@@ -77,19 +77,13 @@ func NewDataRecord(data *Data) *DataRecord {
 
 //rt
 
-func (dataRecord *DataRecord) Serialize(wal bool) []byte {
+func (dataRecord *DataRecord) Serialize() []byte {
 	var bytes []byte
-	if wal || !dataRecord.Data.Tombstone {
-		bytes = dataRecord.fullSerialize()
+	if !dataRecord.Data.Tombstone {
+		bytes = make([]byte, RecordHeaderSize+dataRecord.KeySize+dataRecord.ValueSize)
 	} else {
-		bytes = dataRecord.partialSerialize()
+		bytes = make([]byte, RecordHeaderSize+dataRecord.KeySize+dataRecord.ValueSize-ValueSizeSize)
 	}
-	return bytes
-}
-
-// Serialize the whole DataRecord
-func (dataRecord *DataRecord) fullSerialize() []byte {
-	bytes := make([]byte, RecordHeaderSize+dataRecord.KeySize+dataRecord.ValueSize)
 	// Append the CRC
 	binary.BigEndian.PutUint32(bytes[CrcStart:TimestampStart], dataRecord.Crc)
 	// Append the Timestamp
@@ -102,81 +96,41 @@ func (dataRecord *DataRecord) fullSerialize() []byte {
 	}
 	// Append the Key Size
 	binary.BigEndian.PutUint64(bytes[KeySizeStart:ValueSizeStart], dataRecord.KeySize)
-	// Append the Value Size
-	binary.BigEndian.PutUint64(bytes[ValueSizeStart:KeyStart], dataRecord.ValueSize)
-	// Append the Key
-	copy(bytes[KeyStart:KeyStart+dataRecord.KeySize], dataRecord.Data.Key)
-	// Append the Value
-	copy(bytes[KeyStart+dataRecord.KeySize:], dataRecord.Data.Value)
+
+	if !dataRecord.Data.Tombstone {
+		// Append the Value Size
+		binary.BigEndian.PutUint64(bytes[ValueSizeStart:KeyStart], dataRecord.ValueSize)
+		// Append the Key
+		copy(bytes[KeyStart:KeyStart+dataRecord.KeySize], dataRecord.Data.Key)
+		// Append the Value
+		copy(bytes[KeyStart+dataRecord.KeySize:], dataRecord.Data.Value)
+	} else {
+		// Append the Key
+		copy(bytes[ValueSizeStart:ValueSizeStart+dataRecord.KeySize], dataRecord.Data.Key)
+	}
 
 	return bytes
 }
 
-// Serialize the DataRecord without ValueSize and Value
-func (dataRecord *DataRecord) partialSerialize() []byte {
-	bytes := make([]byte, RecordHeaderSize-ValueSizeSize+dataRecord.KeySize)
-	// Append the CRC
-	binary.BigEndian.PutUint32(bytes[CrcStart:TimestampStart], dataRecord.Crc)
-	// Append the Timestamp
-	binary.BigEndian.PutUint64(bytes[TimestampStart:TombstoneStart], dataRecord.Data.Timestamp)
-	// Append the Tombstone
-	if dataRecord.Data.Tombstone {
-		bytes[TombstoneStart] = 1
-	} else {
-		bytes[TombstoneStart] = 0
-	}
-	// Append the Key Size
-	binary.BigEndian.PutUint64(bytes[KeySizeStart:ValueSizeStart], dataRecord.KeySize)
-	// Append the Key - ValueSizeStart is just there to append the Key after the KeySize, since it was where the Value would be if tombstone was false
-	copy(bytes[ValueSizeStart:], dataRecord.Data.Key)
-
-	return bytes
-}
-
-// Deserialize the whole DataRecord
-func Deserialize(bytes []byte, wal bool) (*DataRecord, error) {
-	var dataRecord *DataRecord
-	var err error
-	if wal {
-		dataRecord, err = fullDeserialize(bytes)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		if bytes[TombstoneStart] == 1 {
-			dataRecord, err = partialDeserialize(bytes)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			dataRecord, err = fullDeserialize(bytes)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-
-	return dataRecord, nil
-}
-
-// Deserialize the DataRecord without it's ValueSize and Value
-func fullDeserialize(bytes []byte) (*DataRecord, error) {
+func Deserialize(bytes []byte) (*DataRecord, error) {
 	crc := binary.BigEndian.Uint32(bytes[CrcStart:TimestampStart])
 	timestamp := binary.BigEndian.Uint64(bytes[TimestampStart:TombstoneStart])
 	tombstone := bytes[TombstoneStart] == 1
 	keySize := binary.BigEndian.Uint64(bytes[KeySizeStart:ValueSizeStart])
-	valueSize := binary.BigEndian.Uint64(bytes[ValueSizeStart:KeyStart])
-	key := string(bytes[KeyStart : KeyStart+keySize])
 
-	var value = make([]byte, valueSize)
-	copy(value, bytes[KeyStart+keySize:])
-
-	// Check if the CRC matches
-	if crc != crc32.ChecksumIEEE(bytes[TimestampStart:]) {
-		return nil, errors.New("CRC does not match")
+	var key string
+	var valueSize uint64
+	var value []byte
+	if !tombstone {
+		valueSize = binary.BigEndian.Uint64(bytes[ValueSizeStart:KeyStart])
+		key = string(bytes[KeyStart : KeyStart+keySize])
+		value = make([]byte, valueSize)
+		copy(value, bytes[KeyStart+keySize:])
+	} else {
+		key = string(bytes[ValueSizeStart : ValueSizeStart+keySize])
 	}
 
-	return &DataRecord{
+	dataRecord := &DataRecord{
 		Data: &Data{
 			Key:       key,
 			Value:     value,
@@ -186,31 +140,13 @@ func fullDeserialize(bytes []byte) (*DataRecord, error) {
 		Crc:       crc,
 		KeySize:   keySize,
 		ValueSize: valueSize,
-	}, nil
-}
-
-func partialDeserialize(bytes []byte) (*DataRecord, error) {
-	crc := binary.BigEndian.Uint32(bytes[CrcStart:TimestampStart])
-	timestamp := binary.BigEndian.Uint64(bytes[TimestampStart:TombstoneStart])
-	tombstone := bytes[TombstoneStart] == 1
-	keySize := binary.BigEndian.Uint64(bytes[KeySizeStart:ValueSizeStart])
-	// The bytes positions are moved when the value and value size aren't stored
-	key := string(bytes[ValueSizeStart : ValueSizeStart+keySize])
+	}
 
 	// Check if the CRC matches
 	if crc != crc32.ChecksumIEEE(bytes[TimestampStart:]) {
-		return nil, errors.New("CRC does not match")
+		// return dataRecord anyway for merkle
+		return dataRecord, errors.New("CRC does not match")
+	} else {
+		return dataRecord, nil
 	}
-
-	return &DataRecord{
-		Data: &Data{
-			Key:       key,
-			Value:     nil,
-			Tombstone: tombstone,
-			Timestamp: timestamp,
-		},
-		Crc:       crc,
-		KeySize:   keySize,
-		ValueSize: 0,
-	}, nil
 }
