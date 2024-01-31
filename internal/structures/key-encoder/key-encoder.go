9137897788ec4f594dc208f5968aee27fc4121c3
Mijat Krivokapić <mijat2004@gmail.com>
2024-01-31 16:35:23 +0100

    implement GetKey for key-encoder

--- internal/structures/key-encoder/key-encoder.go
+++ internal/structures/key-encoder/key-encoder.go
@@ -7,3 +7,14 @@ type KeyEncoder struct {
 func NewKeyEncoder() *KeyEncoder {
 	return &KeyEncoder{Keys: make(map[string]uint64)}
 }
+
+// returns encoded value of given key, if it doesn't already exists it is created
+func (keyEncoder *KeyEncoder) GetKey(key string) uint64 {
+	encoded, ok := keyEncoder.Keys[key] //if key already exists, just return its encoded value
+	if ok {
+		return encoded
+	}
+	encoded = uint64(len(keyEncoder.Keys))
+	keyEncoder.Keys[key] = encoded
+	return encoded
+}
