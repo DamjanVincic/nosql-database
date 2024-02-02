86d6923770657c1492f7b06963a98c9e267d253e
zoricnadja <zoric.n2003@gmail.com>
2024-02-02 22:12:17 +0100

    update write sstable func

--- internal/structures/sstable/sstable.go
+++ internal/structures/sstable/sstable.go
@@ -83,14 +83,17 @@ same struct for SSTable single and multi-file implementation
     if singleFile==true then we create single SSTable file, otherwise multi
 */
 type SSTable struct {
-	indexConst   uint16
-	summaryConst uint16
-	singleFile   bool
-	compression  bool
-	encoder      *keyencoder.KeyEncoder
+	indexConst        uint16
+	summaryConst      uint16
+	singleFile        bool
+	compression       bool
+	encoder           *keyencoder.KeyEncoder
+	leveledCompaction bool
+	firstLevelMax     uint16
+	levelMultiplier   uint64
 }
 
-func NewSSTable(indexSparseConst uint16, summarySparseConst uint16, singleFile bool, compression bool) (*SSTable, error) {
+func NewSSTable(indexSparseConst uint16, summarySparseConst uint16, singleFile bool, compression bool, leveledCompaction bool, firstLevelMax uint16, levelMultiplier uint64) (*SSTable, error) {
 	//check if sstable dir exists, if not create it
 	_, err := os.ReadDir(Path)
 	if os.IsNotExist(err) {
@@ -109,95 +112,79 @@ func NewSSTable(indexSparseConst uint16, summarySparseConst uint16, singleFile b
 	}
 
 	return &SSTable{
-		indexConst:   indexSparseConst,
-		summaryConst: summarySparseConst,
-		singleFile:   singleFile,
-		compression:  compression,
-		encoder:      encoder,
+		indexConst:        indexSparseConst,
+		summaryConst:      summarySparseConst,
+		singleFile:        singleFile,
+		compression:       compression,
+		encoder:           encoder,
+		leveledCompaction: leveledCompaction,
+		firstLevelMax:     firstLevelMax,
+		levelMultiplier:   levelMultiplier,
 	}, nil
 }
 
 // we know which SSTable to create based on the singleFile variable, set in the configuration file
+/* sstable dir contains subdirs for each lsm level
+each subdir contains sstables folders
+one folder = one sstable
+same names for both implementations, no difference
+the difference is in the number of files in sstable folder
+for multi - 5, for single - 1
+path example sstable/01/sst0000200002/data.db   -  data part of the sstable(sst0000200002) placed on the 1st lsm level
+*/
 func (sstable *SSTable) Write(memEntries []*models.Data) error {
-	// Create the ssTable directory (with all ssTable files) if it doesn't exist
-	dirEntries, err := os.ReadDir(Path)
+	// new sstable is always placed on the first lsm level
+	lsmLevel := uint8(1)
+	//index - sequence number of the sstable
+	var index uint16
+	// subdirName : 00 (a two-digit num for the lsm index)
+	subdirName := fmt.Sprintf("%02d", lsmLevel)
+	subdirPath := filepath.Join(Path, subdirName)
+	sstableFolders, err := os.ReadDir(subdirPath)
 	if os.IsNotExist(err) {
-		err := os.Mkdir(Path, os.ModePerm)
+		err := os.Mkdir(subdirPath, os.ModePerm)
+		// if there is no sstable on the lsm level, index for the new one is 1
+		index = 1
 		if err != nil {
 			return err
 		}
 	}
-	var dataFilename string
-	var indexFilename string
-	var summaryFilename string
-	var filterFilename string
-	var metadataFilename string
-
-	// temporarly the lsm index is set to 1
-	lsmIndex := uint8(1)
-	//index - sequence number of the sstable
-	var index uint8
-	/* sstable dir contains subdirs for each ssTable
-	one subdir = one sstable
-	same names for both implementations, no difference
-	the difference is in the number of files in subdir
-	for multi - 5, for single - 1 */
-	var subdirPath string
-	var subdirName string
-
-	// If there are no subdirectoriums in the directory, create the first one
-	if len(dirEntries) == 0 {
-		// subdirName : 00_sstable_00000 (a two-digit num for the lsm index, five-digit num for the index)
-		index = 1
-		subdirName = fmt.Sprintf("%02d_sstable_%05d", lsmIndex, index)
-		subdirPath = filepath.Join(Path, subdirName)
-		err := os.Mkdir(subdirPath, os.ModePerm)
+	// If there are sstables on the lsm level, take index from last added and increment it
+	if len(sstableFolders) != 0 {
+		lastAddedSstableFolderName := sstableFolders[len(sstableFolders)-1].Name()
+		n, err := strconv.ParseUint(lastAddedSstableFolderName[3:], 10, 16)
 		if err != nil {
 			return err
 		}
-	} else {
-		subdirName = dirEntries[len(dirEntries)-1].Name()
-		n, err := strconv.ParseUint(subdirName[11:], 10, 8)
-		if err != nil {
-			return err
-		}
-		index = uint8(n)
+		index = uint16(n)
 		if err != nil {
 			return err
 		}
 		index++
-		subdirName = fmt.Sprintf("%02d_sstable_%05d", lsmIndex, index)
-		subdirPath = filepath.Join(Path, subdirName)
-		err = os.Mkdir(subdirPath, os.ModePerm)
-		if err != nil {
-			return err
-		}
 	}
+
+	sstFolderName := fmt.Sprintf("sst%010d", index)
+	sstFolderPath := filepath.Join(subdirPath, sstFolderName)
+	err = os.Mkdir(sstFolderPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	var filePaths []string
 	// creates files and save the data
 	if sstable.singleFile {
 		// create single file for ssTable
 		// name - single.db
 		filename := fmt.Sprintf("%s.db", SingleFileName)
-
-		err = sstable.createFiles(memEntries, sstable.singleFile, []string{filepath.Join(subdirPath, filename)})
+		filePaths = append(filePaths, filepath.Join(sstFolderPath, filename))
+		err = sstable.createFiles(memEntries, sstable.singleFile, filePaths)
 		if err != nil {
 			return err
 		}
 	} else {
 		// Filename format: PART.db, part = sstable element
 		// create names of new files
-		dataFilename = fmt.Sprintf("%s.db", DataFileName)
-		indexFilename = fmt.Sprintf("%s.db", IndexFileName)
-		summaryFilename = fmt.Sprintf("%s.db", SummaryFileName)
-		filterFilename = fmt.Sprintf("%s.db", FilterFileName)
-		metadataFilename = fmt.Sprintf("%s.db", MetaFileName)
 
-		fileNames := []string{dataFilename, indexFilename, summaryFilename, filterFilename, metadataFilename}
-		var filePaths []string
-		for _, fileName := range fileNames {
-			filePaths = append(filePaths, filepath.Join(subdirPath, fileName))
-		}
-
+		filePaths = makeMultiFilenames(sstFolderPath)
 		//create files
 		err = sstable.createFiles(memEntries, sstable.singleFile, filePaths)
 		if err != nil {
@@ -205,9 +192,31 @@ func (sstable *SSTable) Write(memEntries []*models.Data) error {
 		}
 	}
 	if sstable.compression {
-		return sstable.encoder.WriteToFile()
+		err = sstable.encoder.WriteToFile()
+		if err != nil {
+			return err
+		}
+	}
+	// err = sstable.CheckCompaction(filePaths)
+	if err != nil {
+		return err
 	}
 	return nil
+}
+
+func makeMultiFilenames(subdirPath string) []string {
+	dataFilename := fmt.Sprintf("%s.db", DataFileName)
+	indexFilename := fmt.Sprintf("%s.db", IndexFileName)
+	summaryFilename := fmt.Sprintf("%s.db", SummaryFileName)
+	filterFilename := fmt.Sprintf("%s.db", FilterFileName)
+	metadataFilename := fmt.Sprintf("%s.db", MetaFileName)
+
+	fileNames := []string{dataFilename, indexFilename, summaryFilename, filterFilename, metadataFilename}
+	var filePaths []string
+	for _, fileName := range fileNames {
+		filePaths = append(filePaths, filepath.Join(subdirPath, fileName))
+	}
+	return filePaths
 }
 
 // we distinguish implementations by the singleFile value (and the num of params, 1 for single, 5 for multi)
@@ -459,6 +468,290 @@ func writeToFile(file *os.File, data []byte) error {
 
 	return nil
 }
+
+// func (sstable *SSTable) CheckCompaction(folderEntries []string) error {
+// 	if uint16(len(folderEntries)) > sstable.firstLevelMax {
+// 		//radi kompakciju celog nivoa
+// 		if !sstable.leveledCompaction {
+// 			err := sstable.sizeTieredCompact(folderEntries)
+// 			if err != nil {
+// 				return err
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
+
+// func (sstable *SSTable) sizeTieredCompact(sstables []string) error {
+// 	var sstForCompaction []mmap.MMap
+// 	for _, sstable := range sstables {
+// 		file, err := os.OpenFile(sstable, os.O_RDWR, 0644)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		mmap, err := mmap.Map(file, mmap.RDWR, 0)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		dataBlock := mmap[DataBlockStart:IndexBlockStart]
+// 		sstForCompaction = append(sstForCompaction, dataBlock)
+// 		err = mmap.Unmap()
+// 		if err != nil {
+// 			return err
+// 		}
+// 		err = file.Close()
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	err := sstable.combineSSTables(sstForCompaction)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
+// func (sstable *SSTable) combineSSTables(sstForCompaction []mmap.MMap) error {
+// 	numberOfSSTables := len(sstForCompaction)
+// 	// we keep where we left of in each sstable and last key
+// 	offsets := make([]uint64, numberOfSSTables)
+// 	current := make([]*models.Data, numberOfSSTables)
+
+// 	var currentFile *os.File
+// 	var mmapCurrFile mmap.MMap
+// 	indexData := initializeIndexRecords(sstable.indexConst, sstable.compression)
+// 	// append index thinning const to indexRecords data
+// 	binary.BigEndian.PutUint16(indexData[:IndexConstSize], sstable.indexConst)
+// 	var groupedData = make([]*[]byte, 5)
+// 	var summaryData []byte
+// 	var filterData []byte
+// 	var merkleData []byte
+// 	var filterKeys []string
+// 	groupedData[0] = &indexData
+// 	groupedData[1] = &summaryData
+// 	groupedData[2] = &filterData
+// 	groupedData[3] = &merkleData
+// 	// Storage for offsets in case of single file
+// 	// Storage for offsets in case of single file
+// 	var dataBlockSize uint64
+// 	var indexBlockSize uint64
+// 	var summaryBlockSize uint64
+// 	var filterBlockSize uint64
+// 	var metaBlockSize uint64
+
+// 	var index uint64
+// 	subDirName := fmt.Sprintf("%2d", lsmLevel+1)
+// 	subDirPath := filepath.Join(Path, subDirName)
+// 	subDirEntries, err := os.ReadDir(subDirPath)
+// 	if os.IsNotExist(err) {
+// 		err := os.Mkdir(subDirPath, os.ModePerm)
+// 		index = 1
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	//read name from last added sstable in next lsm level and create new name
+// 	tempIndex, err := strconv.Atoi(subDirEntries[len(subDirEntries)-1].Name()[3:])
+// 	index = uint64(tempIndex + 1)
+// 	sstDirName := fmt.Sprintf("%5d", index)
+// 	sstDirPath := filepath.Join(subDirPath, sstDirName)
+// 	err = os.Mkdir(sstDirPath, os.ModePerm)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	var fileName string
+// 	if singleFile {
+// 		fileName = fmt.Sprintf("%s.db", SingleFileName)
+// 	} else {
+// 		fileName = fmt.Sprintf("%s.db", DataFileName)
+// 	}
+// 	//if its single - currentFile = all data
+// 	//otherwise = currentFile = data file
+// 	currentFile, err = os.OpenFile(filepath.Join(sstDirPath, fileName), os.O_CREATE|os.O_RDWR, 0644)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	mmapCurrFile, err = mmap.Map(currentFile, mmap.RDWR, 0)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// currentFile.Name()
+// 	//fmt.Print(mmapCurrFile)
+
+// 	// read first record from each sstable
+// 	for i := 0; i < numberOfSSTables; i++ {
+// 		dataRecord, err := readDataFromFile(*sstables[i], 1, "", offsets[i], sstable.com)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if dataRecord != nil {
+// 			// set offsets for next
+// 			offsets[i] = uint64(len(dataRecord.Data.Key) + KeySizeSize + ValueSizeSize + len(dataRecord.Data.Value) + TombstoneSize + TimestampSize + CrcSize)
+// 		}
+// 		current[i] = dataRecord
+// 	}
+// 	// find the smallest key
+// 	// we know that the index we return is the sstable we read next (or multiple)
+// 	offset := uint64(0)
+// 	var serializedMinRec []byte
+// 	// needed for offsets and single file header
+// 	var sizeOfDR uint64
+// 	var sizeOfIR uint64
+// 	var sizeOfSR uint64
+// 	var indexOffset uint64 = IndexConstSize
+// 	// counter for index and index summary, for every n index records add one to summary
+// 	countRecords := uint16(0)
+// 	countIndexRecords := uint16(0)
+// 	var merkleHashedRecords []byte
+
+// 	var summaryMin string
+// 	var summaryMax string
+// 	var serializedIndexRecord []byte
+// 	var serializedSummaryRecord []byte
+
+// 	var summaryHeader = make([]byte, SummaryMinSizeSize+SummaryMaxSizeSize+SummaryConstSize)
+
+// 	//create an new merkle tree with new hashfunc and without nodes
+// 	merkleTree := merkle.NewMerkle(nil)
+// 	var dataRecord *models.DataRecord
+// 	for len(sstables) > 0 {
+// 		smallestKey := findSmallestKey(current)
+// 		if smallestKey == nil {
+// 			break
+// 		}
+// 		var minRecord *models.Data
+// 		for _, keyIndex := range smallestKey {
+// 			if minRecord == nil {
+// 				minRecord = current[keyIndex].Data
+// 			}
+// 			if minRecord.Timestamp < current[keyIndex].Data.Timestamp {
+// 				minRecord = current[keyIndex].Data
+// 				dataRecord = models.NewDataRecord(minRecord)
+// 			}
+// 			nextRecord, err := readDataFromFile(*sstables[keyIndex], 1, "", offsets[keyIndex])
+// 			if err != nil {
+// 				return err
+// 			}
+// 			if nextRecord != nil {
+// 				// set offsets for next
+// 				offsets[keyIndex] += uint64(len(dataRecord.Data.Key) + CrcSize + KeySizeSize + ValueSizeSize + len(dataRecord.Data.Value) + TombstoneSize + TimestampSize)
+// 			}
+// 			current[keyIndex] = nextRecord
+// 		}
+// 		filterKeys = append(filterKeys, dataRecord.Data.Key)
+// 		serializedMinRec = dataRecord.Serialize()
+// 		sizeOfDR = uint64(len(serializedMinRec))
+// 		dataBlockSize += sizeOfDR
+// 		hashedData, err := merkleTree.CreateNodeData(dataRecord)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		merkleHashedRecords = append(merkleHashedRecords, hashedData...)
+
+// 		// every Nth one is saved in the index (key, offset of dataRec)
+// 		if countRecords%indexConst == 0 {
+// 			serializedIndexRecord = addToIndex(offset, dataRecord.Data, &indexData)
+// 			sizeOfIR = uint64(len(serializedIndexRecord))
+// 			indexBlockSize += sizeOfIR
+// 			// every Nth one is saved in the summary index (key, offset of indexRec)
+// 			if countIndexRecords%summaryConst == 0 {
+// 				serializedSummaryRecord = addToIndex(indexOffset, dataRecord.Data, &summaryData)
+// 				sizeOfSR = uint64(len(serializedSummaryRecord))
+// 				summaryBlockSize += sizeOfSR
+// 				if summaryMin == "" {
+// 					summaryMin = dataRecord.Data.Key
+// 				}
+// 			}
+// 			indexOffset += sizeOfIR
+// 			countIndexRecords++
+// 		}
+// 		summaryMax = dataRecord.Data.Key
+// 		offset += sizeOfDR
+// 		countRecords++
+
+// 	}
+// 	//create an empty bloom filter
+// 	filter := bloomfilter.CreateBloomFilter(len(filterData), 0.001)
+// 	//add keys to bf
+// 	for _, key := range filterKeys {
+// 		err = filter.AddElement([]byte(key))
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	filterData = filter.Serialize()
+// 	filterBlockSize = uint64(len(filterData))
+
+// 	//metadata
+// 	err = merkleTree.CreateMerkleTree(merkleHashedRecords)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	merkleData = merkleTree.Serialize()
+// 	metaBlockSize = uint64(len(merkleData))
+// 	//creating summary index header
+// 	binary.BigEndian.PutUint16(summaryHeader[:SummaryMinSizeStart], summaryConst)
+// 	binary.BigEndian.PutUint64(summaryHeader[SummaryMinSizeStart:SummaryMaxSizeStart], uint64(len([]byte(summaryMin))))
+// 	binary.BigEndian.PutUint64(summaryHeader[SummaryMaxSizeStart:SummaryMaxSizeStart+SummaryMaxSizeSize], uint64(len([]byte(summaryMax))))
+// 	summaryHeaderSize := SummaryConstSize + SummaryMinSizeSize + SummaryMaxSizeSize + uint64(len([]byte(summaryMin))) + uint64(len([]byte(summaryMax)))
+// 	//append all summary index records
+// 	summaryHeader = append(summaryHeader, []byte(summaryMin)...)
+// 	summaryHeader = append(summaryHeader, []byte(summaryMax)...)
+// 	summaryHeader = append(summaryHeader, summaryData...)
+// 	summaryBlockSize += summaryHeaderSize
+
+// 	if singleFile {
+// 		header := make([]byte, HeaderSize)
+// 		binary.BigEndian.PutUint64(header[:DataBlockSizeSize], dataBlockSize)
+// 		binary.BigEndian.PutUint64(header[IndexBlockStart:SummaryBlockStart], indexBlockSize)
+// 		binary.BigEndian.PutUint64(header[SummaryBlockStart:FilterBlockStart], summaryBlockSize)
+// 		binary.BigEndian.PutUint64(header[FilterBlockStart:MetaBlockStart], filterBlockSize)
+
+// 		fileInfo, err := currentFile.Stat()
+// 		if err != nil {
+// 			return err
+// 		}
+// 		fileSize := fileInfo.Size()
+// 		dataSize := int64(indexBlockSize + summaryBlockSize + filterBlockSize + metaBlockSize)
+// 		if err = currentFile.Truncate(fileSize + dataSize); err != nil {
+// 			return err
+// 		}
+// 		copy(mmapCurrFile, indexData)
+// 		copy(mmapCurrFile, summaryData)
+// 		copy(mmapCurrFile, filterData)
+// 		copy(mmapCurrFile, merkleData)
+// 	} else {
+// 		/*
+// 			for multi file implementation
+// 			write data to dataFile, indexData to indexFile... each block in a separate file
+// 		*/
+// 		filePaths := makeMultiFilenames(subDirPath)
+// 		for idx, fileName := range *filePaths {
+// 			if idx == 0 {
+// 				continue
+// 			}
+// 			file, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0644)
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			err = writeToFile(file, *groupedData[idx])
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			err = file.Close()
+// 			if err != nil {
+// 				return err
+// 			}
+// 		}
+// 	}
+
+// 	currentFile.Close()
+// 	mmapCurrFile.Unmap()
+// 	return nil
+// }
 
 // go through every sstable until you find the key, when its found, return its value
 func (sstable *SSTable) Get(key string) (*models.Data, error) {
