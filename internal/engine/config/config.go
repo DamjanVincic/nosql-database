10e256a239a2d7c726de2714189872dc098d8bc9
Damjan Vincic <damjanvincic2003@gmail.com>
2024-02-02 22:05:33 +0100

    Add memtable config

--- internal/engine/config/config.go
+++ internal/engine/config/config.go
@@ -9,16 +9,28 @@ import (
 const ConfigFile = "config.yaml"
 
 type Config struct {
-	WAL WALConfig `yaml:"WAL"`
+	WAL      WALConfig      `yaml:"wal"`
+	Memtable MemtableConfig `yaml:"memtable"`
 }
 
 type WALConfig struct {
 	SegmentSize uint64 `yaml:"segmentSize" validate:"gte=1"`
 }
 
+type MemtableConfig struct {
+	TableSize      uint64 `yaml:"tableSize" validate:"gte=1"`
+	DataStructure  string `yaml:"dataStructure" validate:"oneof=hashmap skiplist btree"`
+	NumberOfTables uint64 `yaml:"numberOfTables" validate:"gte=1"`
+}
+
 var defaultConfig = &Config{
 	WAL: WALConfig{
 		SegmentSize: 1024 * 1024 * 16,
+	},
+	Memtable: MemtableConfig{
+		TableSize:      1024 * 1024 * 16,
+		DataStructure:  "hashmap",
+		NumberOfTables: 1,
 	},
 }
 
