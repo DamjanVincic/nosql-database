7182d367f5d2b7d9d54d6bf3553f17c62dfe3f4c
Mijat Krivokapić <mijat2004@gmail.com>
2024-02-02 11:25:29 +0100

    implement serializtion and deserialization of key-encoder

--- internal/structures/key-encoder/key-encoder.go
+++ internal/structures/key-encoder/key-encoder.go
@@ -1,6 +1,9 @@
 package key_encoder
 
-import "errors"
+import (
+	"encoding/binary"
+	"errors"
+)
 
 type KeyEncoder struct {
 	Keys map[string]uint64
@@ -33,4 +36,56 @@ func (keyEncoder *KeyEncoder) GetKey(encoded uint64) (key string, err error) {
 	}
 	err = errors.New("couldn't find key with given encoded value in keyEncoder")
 	return
+}
+
+func (keyEncoder *KeyEncoder) Serialize() []byte {
+	bytes := make([]byte, 0)
+	tempBytes := make([]byte, binary.MaxVarintLen64) //temp storage for 64-bit integers
+
+	var tempBytesLength int
+	var keyLength uint64
+
+	for key, encoded := range keyEncoder.Keys {
+		//put encoded key value into tempBytes and get number of bytes written
+		tempBytesLength = binary.PutUvarint(tempBytes, encoded)
+		//append encoded key value to bytes
+		bytes = append(bytes, tempBytes[:tempBytesLength]...)
+
+		keyLength = uint64(len([]byte(key)))
+		//put keyLength into tempBytes and get number of bytes written
+		tempBytesLength = binary.PutUvarint(tempBytes, keyLength)
+		//append key length to bytes
+		bytes = append(bytes, tempBytes[:tempBytesLength]...)
+		//append key to bytes
+		bytes = append(bytes, []byte(key)...)
+	}
+	return bytes
+}
+
+func Deserialize(serializedKeyEncoder []byte) (keyEncoder *KeyEncoder, err error) {
+	keyEncoder = NewKeyEncoder()
+	err = nil
+
+	var tempEncoded uint64 //temp variable for storing encoded key value
+	var tempKeySize uint64 //temp variable for storing keySize
+	var tempKey string     //temp variable used for storing key
+	var tempBytesRead int  //temp variable for number of bytes read for each 64-bit integer
+	bytesRead := 0         //total number of bytes read
+
+	for {
+		tempEncoded, tempBytesRead = binary.Uvarint(serializedKeyEncoder[bytesRead:])
+		if tempBytesRead == 0 {
+			return
+		}
+		bytesRead += tempBytesRead
+		tempKeySize, tempBytesRead = binary.Uvarint(serializedKeyEncoder[bytesRead:])
+		if tempBytesRead == 0 {
+			err = errors.New("missing value for key size")
+			return
+		}
+		bytesRead += tempBytesRead
+		tempKey = string(serializedKeyEncoder[bytesRead : bytesRead+int(tempKeySize)])
+		bytesRead += int(tempKeySize)
+		keyEncoder.Keys[tempKey] = tempEncoded
+	}
 }
